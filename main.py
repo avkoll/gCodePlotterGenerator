4838e78a4faa5fb0baae35540dc3d4dcad6a50a6
gnawtough <avkoller01@hotmail.com>
2024-05-19 18:57:10 -0500

    I think starting_gcode is done

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
+from svgConverter import generate_curves  # Converts a svg file to gcode
+
+
 def generate_2d_plotter_gcode(points):
-    gcode = []
 
-    # Initialize the plotter
-    gcode.append("G21 ; Set units to millimeters")
-    gcode.append("G90 ; Use absolute positioning")
-    gcode.append("G28 X0 Y0 ; Home X and Y axes")  # Home only X and Y axes
-    gcode.append("G4 P2000 ; Wait for 2 seconds to ensure homing is completed")
-    gcode.append("G92 E0 ; Reset extruder position")
-
-    # Move to the starting point
-    start_point = points[0]
-    gcode.append(f"G1 X{start_point[0]:.2f} Y{start_point[1]:.2f} F1500")
 
     # Plot the points
     for point in points[1:]:
         x, y = point
         gcode.append(f"G1 X{x:.2f} Y{y:.2f} F1500")
 
+
+
+    return "\n".join(gcode)
+
+
+def starting_gcode(gcodelist):
+
+    # Initialize the plotter
+    gcodelist.append("G21 ; Set units to millimeters")
+    gcodelist.append("G90 ; Use absolute positioning")
+    gcodelist.append("G28 X0 Y0 ; Home X and Y axes")  # Home only X and Y axes
+    gcodelist.append("G4 P2000 ; Wait for 2 seconds to ensure homing is completed")
+    gcodelist.append("G92 E0 ; Reset extruder position")
+
+    # Move to the starting point
+    gcodelist.append(f"G1 X10 Y10 F1500")
+
+    return "\n".join(gcodelist)
+
+
+def create_shapes():
+    generate_curves("./svgs/star-svgrepo-com.svg")
+
     # Finish the plot
     gcode.append("G28 X0 Y0 ; Home X and Y axes")  # Home only X and Y axes
     gcode.append("G4 P2000 ; Wait for 2 seconds to ensure homing is completed")
     gcode.append("M84 ; Disable motors")
 
-    return "\n".join(gcode)
-
 
 def main():
-    # Define the points to plot a square
-    points = [(10, 10), (10, 50), (50, 50), (50, 10), (10, 10)]
+    gcode = []
 
-    gcode = generate_2d_plotter_gcode(points)
+
+
     with open('plotter_output.gcode', 'w') as f:
         f.write(gcode)
 
--- svgConverter.py
+++ svgConverter.py
@@ -2,12 +2,12 @@ from svg_to_gcode.svg_parser import parse_file
 from svg_to_gcode.compiler import Compiler, interfaces
 
 
-def generate_curves():
+def generate_curves(svgPath):
     # Instantiate a compiler, specifying the interface type and the speed at which the tool should move. pass_depth controls
     # how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
     gcode_compiler = Compiler(interfaces.Gcode, movement_speed=1000, cutting_speed=300, pass_depth=0)
 
-    curves = parse_file("./svgs/star-svgrepo-com.svg")  # Parse an svg file into geometric curves
+    curves = parse_file(svgPath)  # Parse an svg file into geometric curves
 
     gcode_compiler.append_curves(curves)
     gcode_compiler.compile_to_file("drawing.gcode", passes=1)
